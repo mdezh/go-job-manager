d08ed97272f7fbd7c512edda2940298524bb0187
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-10-07 02:55:16 +0300

    minor improvements

--- pkg/manager/exec_job.go
+++ pkg/manager/exec_job.go
@@ -3,14 +3,19 @@ package manager
 import "context"
 
 func (m *manager) execJob(j *jobRecord) {
-	j.setWorking(true)
 	defer m.wg.Done()
+
+	j.setWorking(true)
 	defer j.setWorking(false)
+
+	if m.isStopped() {
+		return
+	}
 
 	ctx, cancel := context.WithTimeout(m.ctx, j.cfg.Timeout())
 	defer cancel()
 
 	if err := j.job(ctx); err != nil {
-		m.logger.Println(j.name, ":", err)
+		m.logger.Println(j.name, err)
 	}
 }
