f780374a0dcbb845a0f8fb4b94868ca943beb6e0
Mikhail Dezhurko <m.m.dezhurko@gmail.com>
2023-10-10 15:18:33 +0300

    recalculate interval on the each tick

--- pkg/manager/job_loop.go
+++ pkg/manager/job_loop.go
@@ -5,20 +5,23 @@ import "time"
 func (m *manager) jobLoop(j *jobRecord) {
 	defer m.wg.Done()
 
-	ticker := time.NewTicker(j.cfg.Interval())
-	defer ticker.Stop()
+	timer := time.NewTimer(j.cfg.Interval())
 
 	m.wg.Add(1)
 	go m.execJob(j)
 
 	for {
 		select {
-		case <-ticker.C:
+		case <-timer.C:
+			// recalculate interval on the each tick
+			timer = time.NewTimer(j.cfg.Interval())
+
 			if !j.isWorking() {
 				m.wg.Add(1)
 				go m.execJob(j)
 			}
 		case <-m.stopped:
+			timer.Stop()
 			return
 		}
 	}
